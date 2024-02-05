f918383ca4cfa70d1112c450e19741623742e8d4
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2024-02-05 21:31:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import "./App.css";
+import { motion } from "framer-motion";
+import { YesModal } from "./components/YesModal";
+import { useState } from "react";
+
+function App() {
+	const [openModal, setOpenModal] = useState(false);
+
+	const yesButtton = () => {
+		setOpenModal(true);
+	};
+
+	const closeButton = (e) => {
+		setOpenModal(false);
+	};
+
+	return (
+		<>
+			<div className="bg-gradient-to-r from-pink-500 to-rose-500 h-screen flex items-center justify-center relative overflow-hidden">
+				{openModal ? (
+					<div className="flex items-center justify-center z-50">
+						<YesModal closeButton={(value) => closeButton(value)} />
+					</div>
+				) : (
+					""
+				)}
+				<motion.img
+					initial={{ y: -1000 }}
+					animate={{ y: 0 }}
+					transition={{ duration: 0.9, delay: 1 }}
+					src="/images/topHearts.png"
+					alt=""
+					className="w-full absolute top-0"
+				/>
+				<motion.div
+					initial={{ y: 1000 }}
+					animate={{ y: 0 }}
+					transition={{ duration: 0.9, delay: 1 }}>
+					<motion.img
+						initial={{ y: 20 }}
+						animate={{ y: 5 }}
+						transition={{
+							duration: 0.9,
+							delay: 1,
+							repeat: Infinity,
+							repeatType: "reverse",
+						}}
+						src="/images/leftHeart.png"
+						alt=""
+						className="w-[700px] absolute -left-56 -bottom-20 overflow-x-hidden"
+					/>
+				</motion.div>
+				<motion.div
+					initial={{ y: 1000 }}
+					animate={{ y: 0 }}
+					transition={{ duration: 0.9, delay: 1 }}
+					className="flex items-start justify-center">
+					<div
+						className="text-center
+          ">
+						<motion.div
+							initial={{ y: 20 }}
+							animate={{ y: 5 }}
+							transition={{
+								duration: 0.9,
+								delay: 1.5,
+								repeat: Infinity,
+								repeatType: "reverse",
+							}}>
+							<h1 className="font-mainFont text-9xl font-semibold text-white m-0">
+								Hi Crush!!!
+							</h1>
+						</motion.div>
+						<div className="w-[600px] pt-2 m-0">
+							<h1 className="font-mainFont text-5xl text-white">
+								Would you like to become my valentine this coming February 14, 2024?
+							</h1>
+						</div>
+						<div className="flex gap-2 items-center justify-center pt-3">
+							<button
+								onClick={yesButtton}
+								className="w-[90px] h-[50px] text-3xl flex items-center justify-center bg-white text-pink-500 font-semibold rounded font-mainFont transform transition-transform hover:scale-110 z-40">
+								Yes
+							</button>
+							<button className="w-[90px] h-[50px] text-3xl flex items-center justify-center bg-red-600 text-white font-semibold rounded font-mainFont hover:-translate-y-72 transform transition-transform ease-in-out duration-300 z-40">
+								No
+							</button>
+						</div>
+					</div>
+					<motion.div
+						initial={{ y: 1000 }}
+						animate={{ y: 0 }}
+						transition={{ duration: 0.9, delay: 1 }}>
+						<motion.img
+							initial={{ y: 20 }}
+							animate={{ y: 5 }}
+							transition={{
+								duration: 0.9,
+								delay: 1,
+								repeat: Infinity,
+								repeatType: "reverse",
+							}}
+							src="/images/rightHeart.png"
+							alt=""
+							className="w-[700px] absolute -right-56 -bottom-20 overflow-x-hidden"
+						/>
+					</motion.div>
+				</motion.div>
+				<motion.div
+					initial={{ y: 1000 }}
+					animate={{ y: 0 }}
+					transition={{ duration: 0.9, delay: 1 }}
+					className="absolute bottom-2 z-40">
+					<a href="https://patrick-dionen.vercel.app/">
+						<p className="text-xs text-white">Made with ❤️ by Patrick James Dionen</p>
+					</a>
+				</motion.div>
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/YesModal.jsx
@@ -0,0 +1,52 @@
+import { motion } from "framer-motion";
+
+export const YesModal = ({ closeButton }) => {
+	const closeButtonModal = () => {
+		closeButton(false);
+	};
+
+	return (
+		<>
+			<motion.div
+				initial={{ opacity: 0, scale: 0.5 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{ duration: 0.5 }}
+				className={`fixed z-40 top-0 left-0 w-full h-screen flex items-center backdrop-filter backdrop-blur-sm justify-center `}>
+				<div className="w-[500px] h-fit bg-white px-10 py-4 rounded">
+					<div className="absolute">
+						<img src="/images/leftHeart.png" alt="" className="opacity-10" />
+					</div>
+					<h1 className="font-mainFont text-3xl">My Dearest Crush,</h1>
+					<p className="font-mainFont text-2xl indent-5">
+						As I sit down to pen this letter, my heart swells with the warmth your
+						presence brings into my life. In every shared smile, stolen glance, and
+						tender moment, I find a depth of connection that words often struggle to
+						capture. Your love is a melody that plays in the background of my days, a
+						constant source of joy and inspiration. It's the gentle whisper in the
+						wind, the sunbeam breaking through the clouds, and the steady rhythm of my
+						heartbeat. In your laughter, I discover the sweetest symphony, and in your
+						embrace, I find the safest haven. Together, we've woven a tapestry of
+						shared dreams, whispered promises, and countless memories. With each
+						passing day, my affection for you grows stronger, deeper, and more
+						profound. Your kindness, your quirks, and the unique way you see the world
+						make my heart dance with gratitude for having you by my side. In this
+						journey of love, I am grateful for the beautiful moments we've created and
+						excited for the many more that await us. Your love is the anchor that
+						grounds me, the compass that guides me, and the light that illuminates my
+						path. This message is by GPT btw.
+					</p>
+					<h1 className="text-2xl font-mainFont">
+						Forever yours, Trikk <span className="text-sm">💋</span>{" "}
+					</h1>
+					<div className="flex items-end justify-end">
+						<button
+							onClick={closeButtonModal}
+							className="bg-gradient-to-r from-pink-500 to-rose-500 outline-none px-4 rounded h-10 text-white z-50">
+							Back
+						</button>
+					</div>
+				</div>
+			</motion.div>
+		</>
+	);
+};
